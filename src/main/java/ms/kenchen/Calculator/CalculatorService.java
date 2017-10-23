d72c8619a7cf7c2a16203cd0ed10a6a2edc99a7e
itzikbekel <itzik@camilyo.com>
2017-10-23 16:20:08 +0300

    Update CalculatorService.java

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_HTML)
     public String ping() {
-        return ("<html> " + "<title>" + "Hello HTML Response" + "</title><body><b>"+"</b></body></html>").build();
+        return ("<html> " + "<title>" + "Hello HTML Response" + "</title><body><b>"+"</b></body></html>");
     }
 
     @GET
