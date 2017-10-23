7ef286cf42d17e41df23f9c24a813a37984f7ce8
itzikbekel <itzik@camilyo.com>
2017-10-23 16:43:12 +0300

    Update CalculatorService.java

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -7,12 +7,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Date;
 
-@Path("/calculator")
+@Path("/goopen")
 public class CalculatorService {
     @GET
-    @Path("ping")
+    @Path("start")
     @Produces(MediaType.TEXT_HTML)
-    public String ping() {
+    public String start() {
       
   return ("<html> " + "<header>" + "<title>This is title</title>" + "</header>" + "<P>" + "<body style='background-color:powderblue';>" + "<font size='18' color='blue'>" + "PROD = Go Open-Source Web Site!" + "</font></p><font size='8' color='blue'>"+ "http://goossweb.azurewebsites.net" + "</font>" + "</html>");
 
